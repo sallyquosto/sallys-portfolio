ecc3268eda3176a92293b34cd78f3107077cfec3
Sally Quosto <sallyquosto@Sallys-MBP.lan1>
2021-05-08 21:42:28 -0400

    added styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,30 +9,30 @@ export default function NavBar() {
         <header position="static" style={{background: "#041d2b"}}>
             <div className="container mx-auto fles justify-between">
                 <nav className="flex">
-                    <NavLink style={{color: "#ea4492"}}
+                    <NavLink 
                     to="/" 
                     activeClassName="text-white"
                     exact className="inline-flex items-center py-6 px-3 mr-4 text-blue-900 hover:text-blue-400 text-4xl font-bold font tracking-widest"
                     >
                         Sally.
                     </NavLink>
-                    <NavLink style={{color: "#ea4492"}}
+                    <NavLink
                     to="/skills"
-                    activeClassName="text-blue-100 bg-blue-500" 
+                    activeClassName="bg-yellow-700" 
                     exact className="inline-flex items-center py-3 px-3 mr-6 rounded text-blue-800 hover:text-blue-400"
                     >
                         Skills.
                     </NavLink>
-                    <NavLink style={{color: "#ea4492"}}
+                    <NavLink 
                     to="/project"
-                    activeClassName="text-blue-100 bg-blue-500"
+                    activeClassName="bg-yellow-700"
                     exact className="inline-flex items-center py-3 px-3 mr-6 rounded text-blue-800 hover:text-blue-400"
                     >
                         Projects.
                     </NavLink>
-                    <NavLink style={{color: "#ea4492"}}
+                    <NavLink 
                     to="/about"
-                    activeClassName="text-blue-100 bg-blue-500"
+                    activeClassName="bg-yellow-700"
                     exact className="inline-flex items-center py-3 px-3 mr-6 rounded text-blue-800 hover:text-blue-400"
                     >
                         About Me.
