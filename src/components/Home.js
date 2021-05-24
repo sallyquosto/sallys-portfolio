3bb2623158f28de4bb473c515c69d0a70123f039
Sally Quosto <sallyquosto@Sallys-MacBook-Pro.local>
2021-05-23 20:42:31 -0400

    fixed type styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,6 +16,9 @@ const Home = () => {
                     <h5>Hello, my name is</h5>
                     <h2>Sally Quosto.</h2>
                     <h4>&lt;Web Developer/&gt;</h4>
+                    
+                </div>
+                <div className="typing">
                     <ReactTypingEffect text={["My passion is creating webpages, and I'm fluent in HTML, CSS, and JavaScript. Working for an organization where I can contribute to the tech world or simply an end-user by creating page structures, keeping the design up-to-date, responsive, and satisfying for the user is a dream of mine, but it takes a lot of creativity and constant learning which I am up for!"]}/>
                 </div>
             </div>
